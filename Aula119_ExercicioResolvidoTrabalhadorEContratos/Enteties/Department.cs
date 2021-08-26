d89d8642584b79cfa479ea7971d046350ad85e67
Diego G. Oliveira <oliveira.diegoti@gmail.com>
2021-08-26 12:09:34 -0300

    parado nos 10min de aula

---
+++ Aula119_ExercicioResolvidoTrabalhadorEContratos/Enteties/Department.cs
@@ -0,0 +1,14 @@
+﻿namespace Aula119_ExercicioResolvidoTrabalhadorEContratos.Enteties
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department() { }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Aula119_ExercicioResolvidoTrabalhadorEContratos/Enteties/Worker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Aula119_ExercicioResolvidoTrabalhadorEContratos.Enteties.Enums;
+
+namespace Aula119_ExercicioResolvidoTrabalhadorEContratos.Enteties
+{
+    class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+    }
+}
