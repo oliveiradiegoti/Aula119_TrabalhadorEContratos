499fc4f5c098b47aab3ab9da3c2835cf4659e141
Diego G. Oliveira <oliveira.diegoti@gmail.com>
2021-08-27 10:05:10 -0300

    Aula 119 ate 10:44

--- Aula119_ExercicioResolvidoTrabalhadorEContratos/Enteties/Worker.cs
+++ Aula119_ExercicioResolvidoTrabalhadorEContratos/Enteties/Worker.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Aula119_ExercicioResolvidoTrabalhadorEContratos.Enteties.Enums;
 
 namespace Aula119_ExercicioResolvidoTrabalhadorEContratos.Enteties
@@ -9,5 +10,39 @@ namespace Aula119_ExercicioResolvidoTrabalhadorEContratos.Enteties
         public WorkerLevel Level { get; set; }
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();
+
+        public Worker () { }
+
+        public Worker(string name, WorkerLevel level, double baseSalary, Department department)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public void AddContract(HourContract contract)
+        {
+            Contracts.Add(contract);
+        }
+
+        public void RemoveContract(HourContract contract)
+        {
+            Contracts.Remove(contract);
+        }
+
+        public double Income(int year, int month)
+        {
+            double sum = BaseSalary;
+            foreach (HourContract contract in Contracts)
+            {
+                if (contract.Date.Year == year && contract.Date.Month == month)
+                {
+                    sum += contract.TotalValue();
+                }
+            }
+            return sum;
+        }
     }
 }
--- Aula119_ExercicioResolvidoTrabalhadorEContratos/Program.cs
+++ Aula119_ExercicioResolvidoTrabalhadorEContratos/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Globalization;
+using Aula119_ExercicioResolvidoTrabalhadorEContratos.Enteties;
+using Aula119_ExercicioResolvidoTrabalhadorEContratos.Enteties.Enums;
 
 namespace Aula119_ExercicioResolvidoTrabalhadorEContratos
 {
@@ -6,6 +9,31 @@ namespace Aula119_ExercicioResolvidoTrabalhadorEContratos
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter department's name: ");
+            string departmentName = Console.ReadLine();
+            Console.Write("Enter worker data:\nName: ");
+            string workerName = Console.ReadLine();
+            Console.Write("Level (Junior/MedLevel/Senior: ");
+            WorkerLevel workerLevel = Enum.Parse<WorkerLevel>(Console.ReadLine());
+            Console.Write("Base salary: ");
+            double workerSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Department dept = new Department(departmentName);
+            Worker worker = new Worker(workerName, workerLevel, workerSalary, dept);
+
+            Console.Write("How many contracts to this worker? ");
+            int numberOfContracts = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= numberOfContracts; i++)
+            {
+                Console.Write($"Enter #{i} contract data:\nDate (DD/MM/YYYY): ");
+                DateTime contractDate = DateTime.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Duration (hours): ");
+                int contractHours = int.Parse(Console.ReadLine());
+                HourContract contract = new HourContract(contractDate, valuePerHour, contractHours);
+                worker.AddContract(contract);
+            }
         }
     }
 }
